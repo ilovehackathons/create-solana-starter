bff8d4b7d0966741e750ff9ba5350156230c1fb3
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2023-03-09 20:45:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# create-solana-starter
+
+## Requirements
+
+```sh
+$ solana --version
+solana-cli 1.14.16 (src:ab6f3bda; feat:3488713414)
+
+$ anchor --version
+anchor-cli 0.26.0
+```
+
+## Usage
+
+`npx create-solana-starter my-solana-app`
+
+## Ignore this
+
+```sh
+mkdir create-solana-starter
+git init
+npm init -y
+```
---
+++ index.js
@@ -0,0 +1,180 @@
+#!/usr/bin/env node
+
+import { /* exec, */ execSync } from "child_process";
+import { chdir } from "process";
+import chalk from "chalk";
+import { writeFileSync } from "fs";
+import path from "path";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const appName = process.argv[2];
+
+if (!appName) {
+  console.error("Please specify the app name:");
+  console.log(
+    `  ${chalk.cyan("create-solana-starter")} ${chalk.green("<app-name>")}`
+  );
+  console.log();
+  console.log("For example:");
+  console.log(
+    `  ${chalk.cyan("npx create-solana-starter")} ${chalk.green(
+      "my-solana-app"
+    )}`
+  );
+  process.exit(1);
+}
+
+console.log(chalk.greenBright(`Running 'anchor init ${appName}'...`));
+console.log(execSync(`anchor init ${appName}`).toString());
+
+console.log(chalk.greenBright(`Stepping into ${appName}...`));
+chdir(appName);
+
+console.log(
+  chalk.greenBright(
+    `Running 'solana-keygen new --no-bip39-passphrase -o wallet.json'...`
+  )
+);
+console.log(
+  execSync(`solana-keygen new --no-bip39-passphrase -o wallet.json`).toString()
+);
+
+console.log(chalk.greenBright(`Running 'anchor build'...`));
+console.log(execSync("anchor build").toString());
+
+const snakeCasedAppName = appName.replace(/\-/g, "_");
+
+console.log(
+  chalk.greenBright(
+    `Running 'solana address -k target/deploy/${snakeCasedAppName}-keypair.json'`
+  )
+);
+let programId;
+console.log(
+  (programId = execSync(
+    `solana address -k target/deploy/${snakeCasedAppName}-keypair.json`
+  )
+    .toString()
+    .trim())
+);
+
+console.log(chalk.greenBright("Generating Anchor.toml..."));
+const ANCHOR_TOML = `
+[programs.localnet]
+${snakeCasedAppName} = "${programId}"
+
+[provider]
+cluster = "Localnet"
+wallet = "wallet.json"
+
+[scripts]
+test = "yarn run ts-mocha -p ./tsconfig.json -t 1000000 tests/**/*.ts"
+
+[test.validator]
+bind_address = "127.0.0.1"
+`.trimStart();
+console.log(ANCHOR_TOML);
+
+console.log(chalk.greenBright("Writing Anchor.toml..."));
+writeFileSync("Anchor.toml", ANCHOR_TOML);
+
+console.log(chalk.greenBright("Generating lib.rs..."));
+const LIB_RS = `
+use anchor_lang::prelude::*;
+
+declare_id!("${programId}");
+
+#[program]
+pub mod ${snakeCasedAppName} {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+`.trimStart();
+console.log(LIB_RS);
+
+console.log(chalk.greenBright("Writing lib.rs..."));
+writeFileSync(path.join("programs", appName, "src", "lib.rs"), LIB_RS);
+
+console.log(chalk.greenBright("Generating package.json..."));
+const PACKAGE_JSON = {
+  scripts: {
+    "lint:fix": 'prettier */*.js "*/**/*{.js,.ts}" -w',
+    lint: 'prettier */*.js "*/**/*{.js,.ts}" --check',
+    verify:
+      "echo 'Did you start solana-test-validator?' && solana airdrop 100000 -u localhost -k wallet.json && anchor deploy && anchor run test",
+  },
+  dependencies: {
+    "@coral-xyz/anchor": "^0.26.0",
+  },
+  devDependencies: {
+    chai: "^4.3.4",
+    mocha: "^9.0.3",
+    "ts-mocha": "^10.0.0",
+    "@types/bn.js": "^5.1.0",
+    "@types/chai": "^4.3.0",
+    "@types/mocha": "^9.0.0",
+    typescript: "^4.3.5",
+    prettier: "^2.6.2",
+  },
+};
+
+console.log(chalk.greenBright("Writing package.json..."));
+writeFileSync("package.json", JSON.stringify(PACKAGE_JSON, null, 2));
+
+console.log(chalk.greenBright("Running 'anchor test'..."));
+console.log(execSync("anchor test").toString());
+
+// console.log(
+//   chalk.greenBright("Running 'solana-test-validator &>/dev/null &'...")
+// );
+// const solanaTestValidator = exec(
+//   "solana-test-validator",
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       // return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   }
+// );
+
+// console.log(
+//   chalk.greenBright("Waiting 5 seconds for solana-test-validator to start...")
+// );
+// await new Promise((resolve) => setTimeout(resolve, 5000));
+
+await readline
+  .createInterface({ input, output })
+  .question(
+    chalk.greenBright("Run ") +
+      chalk.blueBright("solana-test-validator") +
+      chalk.greenBright(" in another terminal and press Enter to continue...")
+  );
+
+console.log(
+  chalk.greenBright(
+    "\nRunning 'solana airdrop 100000 -u localhost -k wallet.json'..."
+  )
+);
+console.log(
+  execSync("solana airdrop 100000 -u localhost -k wallet.json").toString()
+);
+
+console.log(chalk.greenBright("Running 'anchor deploy'..."));
+console.log(execSync("anchor deploy").toString());
+
+console.log(chalk.greenBright("Running 'anchor run test'..."));
+console.log(execSync("anchor run test").toString());
+
+// console.log(chalk.greenBright("Killing solana-test-validator..."));
+// console.log(solanaTestValidator.kill());
+
+console.log(chalk.greenBright("Finished."));
